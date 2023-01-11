d84e9e9c0b30bf9ed43f3f45811ea99deb189c04
Michael Lan <mlan@motorway.co.uk>
2023-01-11 10:42:39 +0000

    Added problem description

--- src/main/java/stacks/ValidParentheses.java
+++ src/main/java/stacks/ValidParentheses.java
@@ -6,6 +6,13 @@ import java.util.Stack;
 public class ValidParentheses {
     public ValidParentheses() {}
 
+    /*
+    Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+    An input string is valid if:
+    1. Open brackets must be closed by the same type of brackets.
+    2. Open brackets must be closed in the correct order.
+    3. Every close bracket has a corresponding open bracket of the same type.
+     */
     public boolean solve(String s) {
         if (s.length() % 2 != 0) {
             return false;
